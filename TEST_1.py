a735ff3b6761b3b12d588be7325c6de04bd47527
PhungNgocDuy <127763027+PhungNgocDuy@users.noreply.github.com>
2026-03-05 15:47:24 +0700

    Second commit

--- TEST_1.py
+++ TEST_1.py
@@ -1,3 +1,4 @@
 print("Hello, my name is Duy")
-print("I am learning Python")
 print("I am enjoying it so far!")
+print("I am learning Python and it's really fun!")
+print("I can't wait to learn more and build cool projects with Python!")
