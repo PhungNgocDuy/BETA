a217395b9a0eae21c2d0f8ae4379ca8273b04940
PhungNgocDuy <127763027+PhungNgocDuy@users.noreply.github.com>
2026-03-05 15:59:11 +0700

    Thirst commit from cao-hoc

--- TEST_1.py
+++ TEST_1.py
@@ -1,4 +1,4 @@
 print("Hello, my name is Duy")
 print("I am enjoying it so far!")
 print("I am learning Python and it's really fun!")
-print("I can't wait to learn more and build cool projects with Python!")
+print("I am excited to see what I can create with Python in the future!")
